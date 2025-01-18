f5e25de9688b7f27b41d3d650eca6cf50774da20
Pingdred <ema.morrone01.em@gmail.com>
2025-01-18 16:27:35 +0100

    Implement speech generation triggers

--- src/openai_voice_engine.py
+++ src/openai_voice_engine.py
@@ -1,12 +1,23 @@
+import sys
+import json
+
 from openai import OpenAI
 
-from cat.mad_hatter.decorators import hook
+from langchain.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableLambda
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.prompts.chat import SystemMessagePromptTemplate
 
-from .settings import VoiceEngineSettings, ResponseType
+from cat.log import log
+from cat.mad_hatter.decorators import hook
+from cat.looking_glass.stray_cat import StrayCat
+from cat.convo.messages import CatMessage, UserMessage
+from cat.utils import langchain_log_output, langchain_log_prompt
+
+from .settings import VoiceEngineSettings, WhenToSpeak
 from .utils import generate_file_name, get_speech_file_path, get_speech_file_url
 
 
-# Text-to-speech function
 def generate_audio_file(text: str, user_id: str, settings: VoiceEngineSettings):
     # Generate speech file name
     file_name = generate_file_name(settings.output_format.value)
@@ -18,7 +29,7 @@ def generate_audio_file(text: str, user_id: str, settings: VoiceEngineSettings):
 
     # Create OpenAi Client
     client = OpenAI(api_key=settings.openai_api_key.get_secret_value())
-    # Chreate Speech
+    # Create Speech
     response = client.audio.speech.create(
         model=settings.quality.value.lower(), voice=settings.voice.value.lower(), input=text
     )
@@ -27,20 +38,84 @@ def generate_audio_file(text: str, user_id: str, settings: VoiceEngineSettings):
     return file_name
 
 
-@hook
-def before_cat_sends_message(message, cat):
-    # Load settings
-    settings = VoiceEngineSettings(**(cat.mad_hatter.get_plugin().load_settings()))
-    # generate speech
-    speech_path = generate_audio_file(message["content"], cat.user_id, settings)
-    # Create speech urls
-    speech_url = get_speech_file_url(user_id=cat.user_id) + speech_path
+def asked_to_speak(message: UserMessage, cat: StrayCat) -> bool:
+    prompt_text = """Your objective is to determine if the user explicitly asked to respond with speech, audio, or voice.
+Respond with the following format, setting the value to true if the user explicitly asked to respond with speech false otherwise:
+{{
+    "speech_requested": true
+}}
+"""
 
-    if settings.response_type == ResponseType.HTML:
-        # Embedd audio in html and update content text
-        html_message = create_html_message(
-            audio_source=speech_url, autoplay=settings.autoplay
-        )
+    prompt = ChatPromptTemplate(
+        messages=[
+            SystemMessagePromptTemplate.from_template(template=prompt_text),
+            *[m.langchainfy() for m in cat.working_memory.history[-3:]],
+            message.langchainfy()
+        ]
+    )
+
+    chain = (prompt 
+        | RunnableLambda(lambda x: langchain_log_prompt(x, "Ask to speak prompt") if log.LOG_LEVEL == "INFO" else x)
+        | cat._llm 
+        | RunnableLambda(lambda x: langchain_log_output(x, "Ask to speak output") if log.LOG_LEVEL == "INFO" else x)
+        # Sometimes some LLMs write True or False instead of true or false
+        | RunnableLambda(lambda x: setattr(x, "content", x.content.lower()) or x)
+        | JsonOutputParser()
+    )
+
+    try:
+        output = chain.invoke({"user_message": message.text})
+    except json.JSONDecodeError:
+        log.error("Unparsable output, setting speech_requested to False")
+        return False
+    except Exception as e:
+        log.error(f"Error while processing output, setting speech_requested to False: {e}")
+        return False
+        
+    return output["speech_requested"]
+
+
+def speech_needed(message: UserMessage, cat: StrayCat) -> bool:
+    settings = VoiceEngineSettings(**(cat.mad_hatter.get_plugin().load_settings()))
+
+    match settings.when_to_speak:
+        case WhenToSpeak.ALWAYS:
+            return True
+        case WhenToSpeak.LONG_TEXT:
+            return len(message.text) >= settings.message_length
+        case WhenToSpeak.SHORT_TEXT:
+            return len(message.text) <= settings.message_length
+        case WhenToSpeak.WHEN_ASKED:
+            return asked_to_speak(message, cat)
+        case _:
+            return False
+
+
+@hook
+def before_cat_reads_message(user_message: UserMessage, cat: StrayCat):
+    # Determine if the cat should respond with speech and store it in working memory
+    cat.working_memory.openai_tts = {
+        "is_speech_needed": speech_needed(user_message, cat)
+    }
+    
+
+@hook(priority=-sys.maxsize)
+def agent_prompt_prefix(prefix: str, cat: StrayCat) -> str:
+    settings = VoiceEngineSettings(**(cat.mad_hatter.get_plugin().load_settings()))
+
+    prefix += "\n\n# Speech Capability: \nSpeech capability is enabled."
+
+    if cat.working_memory.openai_tts["is_speech_needed"]:
+        prefix += " The text you write will be converted to speech.\n"
+
+        # Provide guideline for how to generate text
+        if settings.optimize_text:
+            prefix += (
+                "For a better experience, write only text in a format that can be converted to speech using a text-to-speech engine.\n"
+                "Avoid using characters that cannot be pronounced like emojis, special characters, or code snippets."
+            )
+
+    return prefix
 
 
 @hook(priority=-sys.maxsize)
--- src/settings.py
+++ src/settings.py
@@ -40,23 +40,45 @@ class ResponseType(Enum):
     KEY = "TTS key"
 
 
+class WhenToSpeak(Enum):
+    ALWAYS = "Always"
+    #UNLESS_ASKED_NOT_TO = "Unless asked not to"
+    WHEN_ASKED = "When asked"
+    LONG_TEXT = "Long messages"
+    SHORT_TEXT = "Short messages"
+    
+
 class VoiceEngineSettings(BaseModel):
     openai_api_key: SecretStr = Field(
         title="OpenAI API Key"
     )
-    quality: VoiceQuality = Field(
-        title="Quality", 
-        default=VoiceQuality.STANDARD
+    when_to_speak: WhenToSpeak = Field(
+        title="When to speak",
+        description="Determines speech generation trigger: always, on request, or based on message length.",
+        default=WhenToSpeak.ALWAYS
     )
     voice: Voice = Field(
         title="Voice", 
         default=Voice.ALLOY
     )
+    quality: VoiceQuality = Field(
+        title="Quality", 
+        default=VoiceQuality.STANDARD
+    )
     output_format: SupportedAudioFormat = Field(
         title="Speech format",
         default=SupportedAudioFormat.MP3
     )
+    message_length: int = Field(
+        title="Message length",
+        description="Minimum character count to trigger speech for long or short messages.",
+        default=300
     )
+    optimize_text: bool = Field(
+        title="Optimize text",
+        description="Guide the LLM to generate text more suitable for speech.",
+        default=True
+    )    
     actions: bool = Field(
         title="Actions (Plugin reload required)",
         description="Enable chat interactions to be able to change voices, know current voice or available ones.",
