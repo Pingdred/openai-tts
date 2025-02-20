827c713509e98aa05b50c3d279be48c831640506
Pingdred <ema.morrone01.em@gmail.com>
2025-02-20 12:19:17 +0100

    Add: actions to change speed rates

--- src/actions.py
+++ src/actions.py
@@ -1,10 +1,13 @@
+import json
 import random
+from pathlib import Path
+
 from pydantic import BaseModel, Field
 
 from cat.mad_hatter.mad_hatter import MadHatter
 from cat.mad_hatter.decorators import tool
+from cat.looking_glass.stray_cat import StrayCat
 from cat.experimental.form import form, CatForm
-from .settings import Voice
 
 from .settings import Voice, VoiceSpeed
 from .utils import load_user_settings, save_user_settings
@@ -26,20 +29,21 @@ def pick_random_voice() -> Voice:
     
     return random.choice(voices)
 
-    def pick_random_voice() -> Voice:
-        setting = MadHatter().get_plugin().load_settings()
-        current_voice = setting["voice"]
-        
-        voices = [voice for voice in Voice if voice != current_voice]
-        
-        return random.choice(voices)
 
+def get_next_speed(current_speed: VoiceSpeed, increase: True) -> VoiceSpeed | None:
+    speeds_list = [v for v in VoiceSpeed]
+    current_speed_index = speeds_list.index(current_speed)
 
-    class ChangeVoiceModel(BaseModel):
-        voice: Voice = Field(
-            description=f"The voice to set, should be be one of {[e.value for e in Voice]}.", 
-            default_factory=pick_random_voice
-        )
+    if increase and current_speed_index == len(speeds_list) - 1:
+        return None
+    
+    if not increase and current_speed_index == 0:
+        return None
+    
+    step = 1 if increase else -1
+
+    return speeds_list[current_speed_index + step]
+
 
 class ChangeVoiceModel(BaseModel):
     voice: Voice = Field(
@@ -66,17 +70,70 @@ if is_action_enabled():
         def submit(self, form_data) -> str:
 
             # Load current settings
-            settings = self.cat.mad_hatter.get_plugin().load_settings()
+            settings = load_user_settings(self.cat.user_id)
             # Update voice
-            settings["voice"] = form_data["voice"]
+            settings.voice = form_data["voice"]
             # Save new settings
-            self.cat.mad_hatter.get_plugin().save_settings(settings)
+            save_user_settings(self.cat.user_id, settings)
         
             return {
                 "output": f"Voice changed to {form_data['voice']}."
             }
         
-    @tool(examples=[
+
+    @tool(
+        examples=[
+            "You are speaking too slow",
+            "Can you speak faster?",
+        ]
+    )  
+    def speak_faster(_, cat: StrayCat):
+        """Use this action to make your voice speak faster. Use only if the user has explicitly asked to change the voice speed."""
+        user_settings = load_user_settings(cat.user_id)
+
+        if (new_speed := get_next_speed(user_settings.speed, increase=True)) is None:
+            return "You are already speaking as fast as you can."
+        
+        user_settings.speed = new_speed
+        save_user_settings(cat.user_id, user_settings)
+
+        if new_speed == VoiceSpeed.FAST:
+            return "You are speaking as fast as you can now."
+
+        return "You are now speaking faster."
+
+    
+    @tool(
+        examples=[
+            "You are speaking too fast", 
+            "Can you speak slower?"
+       ]
+    )
+    def speak_slower(_, cat: StrayCat):
+        """Use this action to make the voice speak slower. Use only if the user have explicitly asked to change the voice speed."""
+        settings = load_user_settings(cat.user_id)
+
+        if (new_speed := get_next_speed(settings.speed, increase=False)) is None:
+            return "You are already speaking as slow as you can."
+        
+        settings.speed = new_speed
+        save_user_settings(cat.user_id, settings)       
+        
+        if new_speed == VoiceSpeed.SLOW:
+            return "You are speaking as slow as you can now."
+        
+        return "You are now speaking slower now."
+
+
+    @tool
+    def current_voice_speed(_, cat: StrayCat):
+        """Use this action to know what speed you are currently using. """
+        # Load current settings
+        settings = load_user_settings(cat.user_id)
+
+        return f"The current voice speed is: {settings.speed}."
+
+
     @tool(
         examples=[
             "What's your current voice?",
@@ -87,9 +144,10 @@ if is_action_enabled():
     def current_voice(_, cat: StrayCat):
         """User this action to know what voice you are currently using. """
         # Load current settings
-        settings = cat.mad_hatter.get_plugin().load_settings()
+        settings = load_user_settings(cat.user_id)
 
-        return f"The current voice is {settings['voice']}."
+        return f"The current voice is {settings.voice}."
+
 
     @tool(
         examples=[
